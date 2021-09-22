5f3b3b1a26d62c89ad5816b2c9f29bafeef21b9e
italo.galdino <italo.galdino@basis.com.br>
2021-09-22 11:27:44 -0300

    Commit do projeto.

---
+++ Atividade SO/src/Script.java
@@ -0,0 +1,41 @@
+import com.ibm.jvm.dtfjview.tools.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Script {
+    public static void main(String[] args) throws IOException {
+
+        int numero = 1000000;
+        String valor = "";
+        String backup = "";
+
+        FileWriter arquivo = new FileWriter("arquivo.txt");
+        PrintWriter gravadorArquivo = new PrintWriter(arquivo);
+
+        for (int i = 0; i <= numero; i++){
+
+           valor = ""+i;
+           gravadorArquivo.println(valor);
+
+           if (i % 100000 ==0){
+               System.out.println(valor);
+               arquivo.flush();
+               FileWriter arquivoTemp = new FileWriter("arquivo" + i/100000 +".txt");
+               PrintWriter gravadorTemp = new PrintWriter(arquivoTemp);
+               File copiador = new File("/home/basis/Documentos/Referencia/SGE/Atividade SO/arquivo.txt");
+
+               gravadorTemp.println(valor);
+               arquivoTemp.close();
+
+           }
+        }
+
+
+        arquivo.close();
+
+        System.out.println("Arquvio criado");
+    }
+}
