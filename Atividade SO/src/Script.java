d0c7dbc4241532f4916ddfef14bfcdfcc4f508d0
italo.galdino <italo.galdino@basis.com.br>
2021-09-24 09:42:48 -0300

    Finalização dos experimentos.

--- Atividade SO/src/Script.java
+++ Atividade SO/src/Script.java
@@ -1,6 +1,3 @@
-
-import javax.swing.*;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
--- Atividade SO/src/SubProcessoMain.java
+++ Atividade SO/src/SubProcessoMain.java
@@ -1,10 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class SubProcessoMain {
 
